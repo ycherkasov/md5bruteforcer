b4ab0e9ddf92c923d9f270371920a51d3e6fc778
Yuri Cherkasov <yuri.cherkasov.accounts@gmail.com>
2014-08-25 10:03:06 +0400

    portable itoa

--- include/md5bruteforce/utils.h
+++ include/md5bruteforce/utils.h
@@ -10,7 +10,7 @@
 class string_cast_error : std::runtime_error{
 public:
 	string_cast_error(const char* what) : runtime_error(what){}
-	virtual const char* what() const { return runtime_error::what(); }
+    virtual const char* what() const noexcept { return runtime_error::what(); }
 };
 
 
@@ -38,5 +38,61 @@ void tokenize(const std::string& str, const char token, std::vector<std::string>
     while (std::getline(f, s, token)) {
         out_tokens.push_back(s);
     }
+
+}
+
+void strreverse(char* begin, char* end) {
+
+    char aux;
+
+    while(end>begin)
+
+        aux=*end, *end--=*begin, *begin++=aux;
+
+}
+
+void itoa(int value, char* str, int base) {
+
+    static char num[] = "0123456789abcdefghijklmnopqrstuvwxyz";
+
+    char* wstr=str;
+
+    int sign;
+
+    div_t res;
+
+
+
+    // Validate base
+
+    if (base<2 || base>35){ *wstr='\0'; return; }
+
+
+
+    // Take care of sign
+
+    if ((sign=value) < 0) value = -value;
+
+
+
+    // Conversion. Number is reversed.
+
+    do {
+
+        res = div(value,base);
+
+        *wstr++ = num[res.rem];
+
+    }while(value=res.quot);
+
+    if(sign<0) *wstr++='-';
+
+    *wstr='\0';
+
+
+
+    // Reverse string
+
+    strreverse(str,wstr-1);
 
 }
