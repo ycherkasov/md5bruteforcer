1a783ee4b7364d6373c37702f5b8b4fb0e9ebc23
Yuri Cherkasov <yuri.cherkasov.accounts@gmail.com>
2014-08-25 23:06:37 +0400

    x

---
+++ examples.cmd
@@ -0,0 +1,8 @@
+echo "Calculate password 12345 for provided hash"
+cd ./bin/Release
+brute.exe 3 5 48-57 827ccb0eea8a706c4c34a16891f84e7b
+
+#echo "Calculate password 99999 for provided hash"
+#cd ./bin/Release
+#bin/Release/brute.exe 3 5 48-57 d3eb9a9233e52948740d7eb8c3062d14
+
